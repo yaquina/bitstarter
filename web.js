194af70af471392b8bcc4270a915411dd61fc886
Ubuntu <ubuntu@ip-172-31-29-150.us-west-2.compute.internal>
2013-07-09 23:43:46 +0000

    Read the index.html file.

--- web.js
+++ web.js
@@ -1,10 +1,18 @@
 var express = require('express');
 
+var fs  =  require('fs');
+var BUFSIZE = ( 4 * 1024 );
+var buffer = new Buffer(BUFSIZE);
+var filename = '/home/ubuntu/bitstarter/index.html';
+
+var fdr = fs.openSync( filename, 'r');
+var myHtml = fs.readFileSync(filename);
+fs.closeSync(fdr);
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+  response.send( myHtml );
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
