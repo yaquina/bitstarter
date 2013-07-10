e0af6906769cf9fd897b72a2153e056d13bbb44f
Ubuntu <ubuntu@ip-172-31-29-150.us-west-2.compute.internal>
2013-07-10 02:00:48 +0000

    Added the }) magic.

--- web.js
+++ web.js
@@ -5,16 +5,15 @@ var BUFSIZE = ( 4 * 1024 );
 var buffer = new Buffer(BUFSIZE);
 var filename = '/home/ubuntu/bitstarter/index.html';
 
-var fdr = fs.openSync( filename, 'r');
-var myHtml = fs.readFileSync(filename);
-fs.closeSync(fdr);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send( myHtml + '\n' );
+ response.send(fs.readFileSync( filename, 'utf8'));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
